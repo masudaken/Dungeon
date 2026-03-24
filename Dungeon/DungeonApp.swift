f512ffa80d92c67a2b2599ee7df5b567ad58bbac
Ken Masuda <masudaken@gmail.com>
2026-03-24 10:18:30 +0900

    Initial Commit

---
+++ Dungeon/DungeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonApp.swift
+//  Dungeon
+//
+//  Created by 増田研 on 2026/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DungeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
